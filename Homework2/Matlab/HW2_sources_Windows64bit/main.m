24fbdfcb2b3f6f45d498878292f63fafb89c08df
kalsipp <martin.favre22@gmail.com>
2016-02-21 18:53:49 +0100

    Network in progress

--- Homework2/Matlab/HW2_sources_Windows64bit/main.m
+++ Homework2/Matlab/HW2_sources_Windows64bit/main.m
@@ -16,7 +16,7 @@ n*(2^(1/n)-1)
 %% Task 1-5 printing function
 % Sätt init_pend_three med executiontime och schedulingPolicy
 % Se till att du printar med rätt namn
-
+% Also uncomment the print
 filename = 'Task_3_edf_performance'; 
 
 sim('inv_pend_three');
@@ -30,6 +30,35 @@ xlabel('Time [s]')
 ylabel('\theta [rad]')
 grid on;
 filename = strcat('.\images\',filename)
-print(filename,  '-dpng')
+%print(filename,  '-dpng')
+
+%% Plotta stability area
 
 
+x1 = [];
+x2 = [];
+c = 1;
+%max{0.5-1/klh} < t/h < min{1/klh}
+K = 0:0.1:2;
+for K = 0:0.1:2
+    x1(c) = 1/(K*h)
+    x2(c) = 1/2-1/(K*h)
+    c = c+1;
+end
+plot([0:0.1:2], x1)
+hold on
+plot([0:0.1:2],x2)
+
+
+%% Hitta instabil delay
+close all; clear all;clc;
+sim('inv_pend_delay');
+time = tout;
+data1 = simout.Data(:,1);
+data2 = simout.Data(:,2);
+data3 = simout.Data(:,3);
+plot(time, data1, time, data2, time, data3);
+legend('Delay = 0.01', 'Delay = 0.04', 'Delay = 0.045');
+xlabel('Time [s]');
+ylabel('\Theta [rad]');
+print ('.\images\NET_timedelays', '-dpng');
