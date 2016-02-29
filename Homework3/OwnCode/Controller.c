fe50285ae0f8a7c50089ecaeec672d1ea4551fbf
kalsipp <martin.favre22@gmail.com>
2016-02-29 21:04:18 +0100

    Martins gargantuan taskload

--- Homework3/OwnCode/Controller.c
+++ Homework3/OwnCode/Controller.c
@@ -4,64 +4,72 @@ right = 0;
 
 //We only have goal position
 //Here we calculate the angle from the positions
+dy = yg-y0;
+dx = xg-x0;
+if(dx != 0){
+  goaltheta = atan(dy/dx)*180/M_PI;
+ }
+if(dx < 0){
+  goaltheta += 180;
+ }
+//K = 3.0f;
+//goaltheta = (goaltheta-theta);
+if(goaltheta > 180) goaltheta -= 360;
+if(goaltheta < -180) goaltheta += 360;
+
 dy = yg-y;
 dx = xg-x;
-
-//All this mess is due to atan
-if(dx < 0){ 
-  goaltheta = atan(dy/dx)*180/M_PI;
-  if(dy < 0){
-    goaltheta +=180;
-  }
-  else{
-    goaltheta -=180;
-  } 
- }else{
-  if(dx != 0){
-    goaltheta = atan(dy/dx)*180/M_PI;
-  }else{
-    goaltheta = goaltheta;
-  }
+if(dx != 0){
+  desiredtheta = atan(dy/dx)*180/M_PI;
  }
-
-//Our signal is limited to -180 to 180
-if(goaltheta < 180) goaltheta += 360; 
-if(goaltheta > 180) goaltheta -= 360;
-
+if(dx < 0){
+  desiredtheta += 180;
+ }
+desiredtheta = desiredtheta-theta;
+if(desiredtheta > 180) desiredtheta -= 360;
+if(desiredtheta < -180) desiredtheta += 360;
 
 //Controller for rotation
-K_rot = 0.7f*L/R;
-//K = 3.0f;
-//u_rot = K_rot*(goaltheta -theta);
-
-//Controller for translation
+K_rot = 1.5f*L/R;
 K_trans = 5.0f;
 K_p = 5.0f;
-p = 5.0f;
-d0 = (cos(theta*M_PI/180) *(xg -x) + sin(theta*M_PI/180)* (yg -y));
+p = 1.0f;
+
+
+//Controller for translation
+d0 = (cos(theta*M_PI/180) *(x0 -x) + sin(theta*M_PI/180)*(y0 -y));
+dg = (cos(goaltheta*M_PI/180) *(xg -x) + sin(goaltheta*M_PI/180)*(yg -y));
 dp = sin(goaltheta*M_PI/180)*(x+p*cos(theta*M_PI/180) -x0) -
-  cos(goaltheta*M_PI*180)*(y+p*sin(theta*M_PI/180) -y0);
-u_rot = dp*K_p;
-u_trans = d0*K_trans;
+  cos(goaltheta*M_PI/180)*(y+p*sin(theta*M_PI/180) -y0);
+
+//u_rot = dp*K_p;
+u_trans = 0;//dg*K_trans;
+u_rot = K_rot*desiredtheta;
+
+
 //u_trans = K_trans*d0;
 left += round(u_trans);
 right += round(u_trans);
 
-left += round(-u_rot);
-right +=round(u_rot);
+left += round(-u_rot/2);
+right+= round(u_rot/2);
 
 //Debugprints to MainWindow.
-Serial.print("OWN:");
-Serial.print(goaltheta-theta, DEC);
-Serial.print(";");
 Serial.print(d0, DEC);
-Serial.print(";\n");
-Serial.print("goaltheta=(degree)");
+Serial.print("\n;");
+
+Serial.print("goaltheta=(degree);\n");
 Serial.print(goaltheta, DEC);
 Serial.print(";\n");
-Serial.print("sin theta");
-Serial.print(sin(theta), DEC);
+Serial.print("theta;\n");
+Serial.print(theta, DEC);
 Serial.print(";\n");
-Serial.print("cos theta");
-Serial.print(cos(theta), DEC);
+Serial.print("cos");
+Serial.print(cos(theta*M_PI/180), DEC);
+Serial.print(";\n");
+Serial.print("sin");
+Serial.print(sin(theta*M_PI/180), DEC);
+Serial.print(";\n");
+Serial.print("atan(dy/dx)");
+Serial.print(atan(dy/dx)*180/M_PI, DEC);
 Serial.print(";\n");
---
+++ Homework3/matlab/task8t11.m
@@ -0,0 +1,73 @@
+%% Task 8
+
+close all; clear all; clc;
+
+% Forward and Rotates first row is removed to be able to read. Probably not
+% relevant.  %[t, x, y, theta]
+indata = dlmread('logtask8.txt', ';');
+forward_t = indata(:,1); % time
+forward_x = indata(:,2);
+forward_y = indata(:,3);
+forward_theta = indata(:,4);
+
+plot(forward_t, forward_theta)
+ylabel('angle \theta [degrees]')
+xlabel('time [s]')
+print ('.\images\task8_angleplot', '-dpng')
+close all;
+plot(forward_t, forward_x, forward_t, forward_y)
+xlabel('time [s]')
+ylabel('distance [m]')
+legend('x', 'y')
+print ('.\images\task8_posplot', '-dpng')
+
+
+%% Task 10
+close all; clear all; clc;
+
+% Forward and Rotates first row is removed to be able to read. Probably not
+% relevant.  %[t, x, y, theta]
+indata = dlmread('logtask10step.txt', ';');
+forward_t = indata(:,1); % time
+forward_x = indata(:,2);
+forward_y = indata(:,3);
+forward_theta = indata(:,4);
+
+plot(forward_t, forward_x, forward_t, forward_y)
+xlabel('time [s]')
+ylabel('distance [m]')
+legend('x', 'y')
+print ('.\images\task10_stepplot', '-dpng')
+
+indata = dlmread('logtask10still.txt', ';');
+forward_t = indata(:,1); % time
+forward_x = indata(:,2);
+forward_y = indata(:,3);
+forward_theta = indata(:,4);
+
+plot(forward_t, forward_x, forward_t, forward_y)
+xlabel('time [s]')
+ylabel('distance [m]')
+legend('x', 'y')
+print ('.\images\task10_stillplot', '-dpng')
+
+%% Task 11
+
+close all; clear all; clc;
+
+% Forward and Rotates first row is removed to be able to read. Probably not
+% relevant.  %[t, x, y, theta]
+indata = dlmread('logtask11.txt', ';');
+t = indata(:,1); % time
+theta = indata(:,2);
+d0 = indata(:,3);
+
+plot(t, theta)
+xlabel('time [s]')
+ylabel('angle \theta [degrees]')
+print ('.\images\task11_angleplot', '-dpng')
+
+plot(t, d0)
+xlabel('time [s]')
+ylabel('distance [m]')
+print ('.\images\task11_d0plot', '-dpng')
